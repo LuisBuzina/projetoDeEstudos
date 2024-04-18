787e6246d89f28f804ca5151a50a65222330a638
LuisBuzina <luisbuzina@gmail.com>
2024-04-18 14:47:15 -0300

    NovosArquivo

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function categoria() {
+
+    //fazer um codigo para selecionar a categoria
+
+    /*let btn = window.document.querySelector("input#btn")
+     switch(btn){
+
+        case 0:
+
+            break //caso esqueca vai executar tudo depois
+    } */
+
+    window.alert("Funcionou")
+}
